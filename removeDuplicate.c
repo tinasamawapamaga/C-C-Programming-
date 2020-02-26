d83fa159bbf711b3bf6adf5391c1a260f3542f21
Tin Le <tlv144199@gmail.com>
2020-02-26 17:55:55 -0600

    Create removeDuplicate.c

---
+++ removeDuplicate.c
@@ -0,0 +1,34 @@
+//C program to remove duplicates from a given array and print
+//the new unsorted array without the duplicates
+#include <stdio.h>
+void clearZero(int arr[],int size, int dup);
+
+int main(void) {
+  int array1[] = {10, 0, 30, 40, 1, 0, 20, 5};
+  
+  int size=sizeof(array1)/sizeof(array1[0]);
+  
+  clearZero(array1,size,0);
+  return 0;
+}
+
+void clearZero(int arr[],int size,int dup){
+  int size2=0;
+  int dupl=0;
+  int temp[size];
+
+  for (int i=0; i<size ;i++){
+      if (arr[i] != dup ) {
+        temp[size2] = arr[i];
+        size2++;
+      } else{
+        size++;
+        dupl++;
+      } 
+
+  }
+  for (int i=0 ;i < size2-dupl; i++){
+      printf("%d ",temp[i]);
+  }
+
+}
